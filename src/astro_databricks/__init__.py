00fa04345d87fb122135f02a026f3291c33f4487
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2023-03-10 17:58:20 +0000

    Improve the documentation for release 0.1.0 (#13)
    Fix #6
    
    Some things which were done as part of this commit:
    * expose the `TaskGroup` and the `Operator` from `astro_databricks`
    * move `example_dags` to the root directory

--- src/astro_databricks/__init__.py
+++ src/astro_databricks/__init__.py
@@ -1,3 +1,10 @@
 """Description of the package"""
+from astro_databricks.operators.notebook import DatabricksNotebookOperator
+from astro_databricks.operators.workflow import DatabricksWorkflowTaskGroup
 
 __version__ = "0.1.0"
+__all__ = [
+    "DatabricksNotebookOperator",
+    "DatabricksWorkflowTaskGroup"
+]
+
