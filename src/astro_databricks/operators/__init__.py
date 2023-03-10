0e62db47f5da16a4663bbd23366e87cb5d57d3b2
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2023-03-10 16:56:41 +0000

    Expose taskgroup and operator from astro_databricks

--- src/astro_databricks/operators/__init__.py
+++
@@ -1,7 +0,0 @@
-from astro_databricks.operators.notebook import DatabricksNotebookOperator
-from astro_databricks.operators.workflow import DatabricksWorkflowTaskGroup
-
-__all__ = [
-    "DatabricksNotebookOperator",
-    "DatabricksWorkflowTaskGroup"
-]
