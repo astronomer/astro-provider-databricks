dcd8a2da8119860529860c50ad989173e3624e2c
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2023-05-11 12:29:30 +0100

    Release 0.1.3a2

--- src/astro_databricks/__init__.py
+++ src/astro_databricks/__init__.py
@@ -2,5 +2,5 @@
 from astro_databricks.operators.notebook import DatabricksNotebookOperator
 from astro_databricks.operators.workflow import DatabricksWorkflowTaskGroup
 
-__version__ = "0.1.3a1"
+__version__ = "0.1.3a2"
 __all__ = ["DatabricksNotebookOperator", "DatabricksWorkflowTaskGroup"]
