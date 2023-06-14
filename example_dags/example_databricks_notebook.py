04f7de5e4667db06687d8ecd79313c45816db806
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2023-06-14 23:28:32 +0100

    Remove retries from Databricks tasks. Users are expected to use the repair functionality

--- example_dags/example_databricks_notebook.py
+++ example_dags/example_databricks_notebook.py
@@ -9,7 +9,7 @@ from astro_databricks import DatabricksNotebookOperator
 EXECUTION_TIMEOUT = int(os.getenv("EXECUTION_TIMEOUT", 6))
 default_args = {
     "execution_timeout": timedelta(hours=EXECUTION_TIMEOUT),
-    "retries": int(os.getenv("DEFAULT_TASK_RETRIES", 2)),
+    "retries": int(os.getenv("DEFAULT_TASK_RETRIES", 0)),
     "retry_delay": timedelta(seconds=int(os.getenv("DEFAULT_RETRY_DELAY_SECONDS", 60))),
 }
 
