0a11e2581c3f418d3291bbdafd3f96b7f43c0526
ssyykim <ssyykim@pubg.com>
2023-07-14 14:41:39 +0900

    Added operator that supports all task types

--- src/astro_databricks/__init__.py
+++ src/astro_databricks/__init__.py
@@ -1,6 +1,9 @@
 """Description of the package"""
 from astro_databricks.operators.notebook import DatabricksNotebookOperator
 from astro_databricks.operators.workflow import DatabricksWorkflowTaskGroup
+from astro_databricks.operators.common import DatabricksTaskOperator
 
-__version__ = "0.1.4"
-__all__ = ["DatabricksNotebookOperator", "DatabricksWorkflowTaskGroup"]
+
+
+__version__ = "0.1.5"
+__all__ = ["DatabricksNotebookOperator", "DatabricksWorkflowTaskGroup", "DatabricksTaskOperator"]
