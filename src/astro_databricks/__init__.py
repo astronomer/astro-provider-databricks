5b631cf9401d00a64f2896d885774d3dae42d0a5
Pankaj Koti <pankajkoti699@gmail.com>
2024-03-20 20:19:47 +0530

    Release 0.2.0

--- src/astro_databricks/__init__.py
+++ src/astro_databricks/__init__.py
@@ -3,7 +3,7 @@ from astro_databricks.operators.common import DatabricksTaskOperator
 from astro_databricks.operators.notebook import DatabricksNotebookOperator
 from astro_databricks.operators.workflow import DatabricksWorkflowTaskGroup
 
-__version__ = "0.1.6"
+__version__ = "0.2.0"
 __all__ = [
     "DatabricksNotebookOperator",
     "DatabricksWorkflowTaskGroup",
